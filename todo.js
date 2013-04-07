de46ef077e0b55eb1b17beedda09ff01577905cc
Bart Wiegmans <bartwiegmans@gmail.com>
2013-04-07 14:44:14 +0200

    Simplified the template handling

--- todo.js
+++ todo.js
@@ -1,46 +1,42 @@
 ;(function() {
-    var form = document.getElementById('todo-form');
-    var list = document.getElementById('todo-list');
-    var template = document.getElementById('todo-template');
+    var list = document.getElementById('todo');
+    var template = document.getElementById('template');
+    var input = document.getElementById('add');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        addItem(this.elements['todo-item'].value);
-        this.reset();
+    input.addEventListener('change', function() {
+        addItem(this.value);
+        this.value = '';
     });
 
     function addItem(value) {
         var item = template.cloneNode(true);
-        item.paragraph = item.getElementsByTagName('p').item(0);
-        item.checkbox = item.getElementsByTagName('input').item(0);
+        item.checkbox = item.children[0];
+        item.paragraph = item.children[1];
+        item.input = item.children[2];
         item.id = '';
+        item.input.id = '';
+        item.className = 'todo';
+        item.paragraph.appendChild(document.createTextNode(value));
         item.checkbox.addEventListener('change', handleCheckbox);
         item.paragraph.addEventListener('click', editText);
-        item.paragraph.appendChild(document.createTextNode(value));
+        item.input.addEventListener('change', commitText);
+        item.input.addEventListener('blur', commitText);
         list.appendChild(item);
     }
 
     function editText() {
         var item = this.parentNode;
-        item.input = document.createElement('input');
-        item.input.value = this.firstChild.nodeValue;
         item.className = 'edit';
-        item.appendChild(item.input);
-        item.input.addEventListener('change', commitText);
-        item.input.addEventListener('blur', commitText);
+        item.input.value = this.firstChild.nodeValue;
+        item.input.focus();
     }
 
     function commitText(event) {
         var item = this.parentNode;
-        event.stopPropagation();
         item.paragraph.replaceChild(document.createTextNode(this.value),
                                     item.paragraph.firstChild);
-        item.className = '';
-        setTimeout(function() {
-            item.removeChild(item.input);
-            item.input = null;
-        }, 10);
-        
+        item.input.blur();
+        item.className = 'todo';
     }
 
     function handleCheckbox() {
