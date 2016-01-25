d91833cbfd34ec72972f652696fce6aa994bf264
Bart Wiegmans <bartwiegmans@gmail.com>
2016-01-25 18:04:46 +0100

    Fix rot13
    Use dx for holding our byte to deal with overflows

--- asm/rot13.s
+++ asm/rot13.s
@@ -1,23 +1,58 @@
 /* file header */
 .intel_syntax noprefix
-.section text
-	
+.section .text
 
+/* our main function, equivalent to sub roman_numeral { */
 .globl rot13_encrypt
 .type rot13_encrypt @function
-	/* our main function, equivalent to sub roman_numeral { */
-        .globl roman_numeral
-        .type  roman_numeral @function
-roman_numeral:
-        /* function prologue */
-        mov rax, 42
-        ret
 
 rot13_encrypt:
-	/* prologue and epilogue */
+        /* prologue and epilogue */
         push rbp
         mov rbp, rsp
-        
+        /* lets make sure we have memory available */
+        push rdi
+        call strlen@PLT /* count chars */
+        inc rax /* add one byte */
+        mov rdi, rax /* result becomes argument */
+        call malloc@PLT
+        pop rdi
+        mov rcx, 0 /* set loop variable to 0 */
+        xor rdx, rdx
+_loop:
+        movzx dx, byte ptr [rdi+rcx*1]
+        test dx, dx
+        jz _end
+        /* must be larger than 'A' and smaller than 'z' */
+        cmp dx, 'A'
+        jl _next
+        cmp dx, 'Z'
+        jle _uppercase
+
+        cmp dx, 'a'
+        jl _next
+        cmp dx, 'z'
+        jg _next
+
+_lowercase:
+        add dx, 13
+        cmp dx, 'z'
+        jng _next
+        sub dx, 26
+        jmp _next
+
+_uppercase:
+        add dx, 13
+        cmp dx, 'Z'
+        jle _next
+        sub dx, 26
+
+_next:
+        mov byte ptr [rax+rcx*1], dl
+        inc rcx
+        jmp _loop
+_end:
+        mov byte ptr [rax+rcx*1], 0
         mov rsp, rbp
         pop rbp
         ret
--- asm/test_rot13.c
+++ asm/test_rot13.c
@@ -10,7 +10,7 @@ int main (int argc, char **argv) {
         return 1;
     }
     result = rot13_encrypt(argv[1]);
-    puts(result);
+    printf("%s\n", result);
     free(result);
     return 0;
 }
